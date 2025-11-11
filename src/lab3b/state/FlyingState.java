b5ca808bac6801bc069ce544e69261e176011db7
Joro Georgiev <georgiggeorgiev@tu-sofia.bg>
2025-11-11 15:27:30 +0200

    edit todo text in FlyingState

--- src/lab3b/state/FlyingState.java
+++ src/lab3b/state/FlyingState.java
@@ -3,7 +3,7 @@ package lab3b.state;
 public class FlyingState implements DroneState {
     @Override
     public void handle(DroneContext context, double distance) {
-        // TODO Print the flight distance from the navigate method
+        // TODO Print the flight time from the navigate method
         // TODO Then set the state to Idle
     }
 }
